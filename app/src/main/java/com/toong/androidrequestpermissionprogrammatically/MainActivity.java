93362ecc981cb34b98d7669151f8b81ef2367fef
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-11-30 16:29:47 +0700

    update request permission handler class

--- app/src/main/java/com/toong/androidrequestpermissionprogrammatically/MainActivity.java
+++ app/src/main/java/com/toong/androidrequestpermissionprogrammatically/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 import com.toong.androidrequestpermissionprogrammatically.handler.RequestPermissionHandler;
 
@@ -14,21 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mRequestPermissionHandler = new RequestPermissionHandler();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClicked();
+            }
+        });
+    }
+
+    private void handleButtonClicked(){
         mRequestPermissionHandler.requestPermission(this, new String[] {
                 Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_EXTERNAL_STORAGE
         }, 123, new RequestPermissionHandler.RequestPermissionListener() {
             @Override
             public void onSuccess() {
-                Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "request permission success", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailed() {
-                Toast.makeText(MainActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "request permission failed", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
     @Override
--- app/src/main/java/com/toong/androidrequestpermissionprogrammatically/handler/RequestPermissionHandler.java
+++ app/src/main/java/com/toong/androidrequestpermissionprogrammatically/handler/RequestPermissionHandler.java
@@ -5,6 +5,8 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by PhanVanLinh on 30/08/2017.
@@ -16,33 +18,45 @@ public class RequestPermissionHandler {
     private RequestPermissionListener mRequestPermissionListener;
     private int mRequestCode;
 
-    public void requestPermission(Activity activity, @NonNull String[] permissions,
-            int requestCode, RequestPermissionListener listener) {
+    public void requestPermission(Activity activity, @NonNull String[] permissions, int requestCode,
+            RequestPermissionListener listener) {
         mActivity = activity;
         mRequestCode = requestCode;
         mRequestPermissionListener = listener;
 
-        if (notNeedRequestRuntimePermissions()) {
+        if (!needRequestRuntimePermissions()) {
             mRequestPermissionListener.onSuccess();
             return;
         }
-        if (isPermissionsGranted(permissions)) {
+        requestUnGrantedPermissions(permissions, requestCode);
+    }
+
+    private boolean needRequestRuntimePermissions() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    }
+
+    private void requestUnGrantedPermissions(String[] permissions, int requestCode) {
+        String[] unGrantedPermissions = findUnGrantedPermissions(permissions);
+        if (unGrantedPermissions.length == 0) {
             mRequestPermissionListener.onSuccess();
             return;
         }
-        ActivityCompat.requestPermissions(mActivity, permissions, requestCode);
+        ActivityCompat.requestPermissions(mActivity, unGrantedPermissions, requestCode);
     }
 
-    private boolean notNeedRequestRuntimePermissions() {
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M;
+    private boolean isPermissionGranted(String permission) {
+        return ActivityCompat.checkSelfPermission(mActivity, permission)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
-    private boolean isPermissionsGranted(@NonNull String[] permissions) {
-        int result = 0;
+    private String[] findUnGrantedPermissions(String[] permissions) {
+        List<String> unGrantedPermissionList = new ArrayList<>();
         for (String permission : permissions) {
-            result += ActivityCompat.checkSelfPermission(mActivity, permission);
+            if (!isPermissionGranted(permission)) {
+                unGrantedPermissionList.add(permission);
+            }
         }
-        return result == PackageManager.PERMISSION_GRANTED;
+        return unGrantedPermissionList.toArray(new String[0]);
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
@@ -64,6 +78,7 @@ public class RequestPermissionHandler {
 
     public interface RequestPermissionListener {
         void onSuccess();
+
         void onFailed();
     }
 }
